573fbc2f61246fc593e007d97e494dc36a0cfda5
crzcreate <2452809832@qq.com>
2025-03-09 18:17:17 +0800

    Update panorama_data_utils.py

--- tools/dataset_converters/panorama_data_utils.py
+++ tools/dataset_converters/panorama_data_utils.py
@@ -5,6 +5,7 @@ from os import path as osp
 import mmcv
 import mmengine
 import numpy as np
+import os
 from scipy import io as sio
 #
 #
@@ -71,62 +72,34 @@ class PanoramaData(object):
         use_v1 (bool, optional): Whether to use v1. Default: False.
     """
 
-    def __init__(self, root_path, split='train', use_v1=False):
+    def __init__(self, root_path, split='Area_1',use_v1=False):
         self.root_dir = root_path
         self.split = split
-        self.split_dir = osp.join(root_path,split)
-        self.classes = [
-            'ceiling', 'floor', 'wall', 'beam', 'column', 'window', 'door',
-            'table', 'chair', 'sofa', 'bookcase', 'board', 'clutter'
-        ]
-        self.cat2label = {cat: self.classes.index(cat) for cat in self.classes}
-        self.label2cat = {
-            label: self.classes[label]
-            for label in range(len(self.classes))
+        self.data_dir = osp.join(root_path,
+                                 'Stanford3dDataset_v1.2_Aligned_Version')
+
+        # Following `GSDN <https://arxiv.org/abs/2006.12356>`_, use 5 furniture
+        # classes for detection: table, chair, sofa, bookcase, board.
+        self.cat_ids = np.array([7, 8, 9, 10, 11])
+        self.cat_ids2class = {
+            cat_id: i
+            for i, cat_id in enumerate(list(self.cat_ids))
         }
-        assert split in ['train', 'val', 'test']
-        split_file = osp.join(root_path, split)
-        # split_file = osp.join(self.split_dir, f'{split}_data_idx.txt')
-        # mmengine.check_file_exist(split_file)
-        self.sample_id_list = map(int, mmengine.list_from_file(split_file))
-        self.image_dir = osp.join(self.split_dir, 'img')
+        assert split in [
+            'Area_1', 'Area_2', 'Area_3', 'Area_4', 'Area_5', 'Area_6'
+        ]
+        self.sample_id_list = os.listdir(osp.join(self.data_dir,
+                                                  split))  # conferenceRoom_1
+        for sample_id in self.sample_id_list:
+            if os.path.isfile(osp.join(self.data_dir, split, sample_id)):
+                self.sample_id_list.remove(sample_id)
+        self.image_dir = osp.join(self.split, 'image')
         # self.calib_dir = osp.join(self.split_dir, 'calib')
         # self.depth_dir = osp.join(self.split_dir, 'depth')
-        if use_v1:
-            self.label_dir = osp.join(self.split_dir, 'label_v1')
-        else:
-            self.label_dir = osp.join(self.split_dir, 'label_cor')
+
 
     def __len__(self):
         return len(self.sample_id_list)
-
-    def get_image(self, idx):
-        img_filename = osp.join(self.image_dir, f'{idx:06d}.png')
-        return mmcv.imread(img_filename)
-
-    def get_image_shape(self, idx):
-        image = self.get_image(idx)
-        return np.array(image.shape[:2], dtype=np.int32)
-
-    # def get_depth(self, idx):
-    #     depth_filename = osp.join(self.depth_dir, f'{idx:06d}.mat')
-    #     depth = sio.loadmat(depth_filename)['instance']
-    #     return depth
-
-    # def get_calibration(self, idx):
-    #     calib_filepath = osp.join(self.calib_dir, f'{idx:06d}.txt')
-    #     lines = [line.rstrip() for line in open(calib_filepath)]
-    #     Rt = np.array([float(x) for x in lines[0].split(' ')])
-    #     Rt = np.reshape(Rt, (3, 3), order='F').astype(np.float32)
-    #     K = np.array([float(x) for x in lines[1].split(' ')])
-    #     K = np.reshape(K, (3, 3), order='F').astype(np.float32)
-    #     return K, Rt
-
-    def get_label_objects(self, idx):
-        label_filename = osp.join(self.label_dir, f'{idx:06d}.txt')
-        lines = [line.rstrip() for line in open(label_filename)]
-        # objects = [PanoramaInstance(line) for line in lines]
-        return lines
 
     def get_infos(self, num_workers=1, has_label=True, sample_id_list=None):
         """Get data infos.
@@ -147,89 +120,229 @@ class PanoramaData(object):
 
         def process_single_scene(sample_idx):
             print(f'{self.split} sample_idx: {sample_idx}')
-            # convert depth to points
-            # SAMPLE_NUM = 50000
-            # TODO: Check whether can move the point
-            #  sampling process during training.
-            #这里是点云的读取还有深度图
-            # pc_upright_depth = self.get_depth(sample_idx)
-            # pc_upright_depth_subsampled = random_sampling(
-            #     pc_upright_depth, SAMPLE_NUM)
-
             info = dict()
-            # pc_info = {'num_features': 6, 'lidar_idx': sample_idx}
-            # info['point_cloud'] = pc_info
-            #
-            # mmengine.mkdir_or_exist(osp.join(self.root_dir, 'points'))
-            # pc_upright_depth_subsampled.tofile(
-            #     osp.join(self.root_dir, 'points', f'{sample_idx:06d}.bin'))
-
-            # info['pts_path'] = osp.join('points', f'{sample_idx:06d}.bin')
-            img_path = osp.join('image', f'{sample_idx:06d}.png')
-            image_info = {
-                'image_idx': sample_idx,
-                'image_shape': self.get_image_shape(sample_idx),
-                'image_path': img_path
+            pc_info = {
+                'num_features': 6,
+                'lidar_idx': f'{self.split}_{sample_idx}'
             }
-            info['image'] = image_info
-            #这里是相机矫正数据
-            # K, Rt = self.get_calibration(sample_idx)
-            # calib_info = {'K': K, 'Rt': Rt}
-            # info['calib'] = calib_info
+            info['point_cloud'] = pc_info
+            pts_filename = osp.join(self.root_dir, 's3dis_data',
+                                    f'{self.split}_{sample_idx}_point.npy')
+            pts_instance_mask_path = osp.join(
+                self.root_dir, 's3dis_data',
+                f'{self.split}_{sample_idx}_ins_label.npy')
+            pts_semantic_mask_path = osp.join(
+                self.root_dir, 's3dis_data',
+                f'{self.split}_{sample_idx}_sem_label.npy')
 
-            if has_label:
-                obj_list = self.get_label_objects(sample_idx)
-                instances = {}
-                # annotations['gt_num'] = len([
-                #     obj.classname for obj in obj_list
-                #     if obj.classname in self.cat2label.keys()
-                # ])
-                # if annotations['gt_num'] != 0:
-                #     annotations['name'] = np.array([
-                #         obj.classname for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ])
-                #     annotations['bbox'] = np.concatenate([
-                #         obj.box2d.reshape(1, 4) for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ],
-                #                                          axis=0)
-                #     annotations['location'] = np.concatenate([
-                #         obj.centroid.reshape(1, 3) for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ],
-                #                                              axis=0)
-                #     annotations['dimensions'] = 2 * np.array([
-                #         [obj.length, obj.width, obj.height] for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ])  # lwh (depth) format
-                #     annotations['rotation_y'] = np.array([
-                #         obj.heading_angle for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ])
-                #     annotations['index'] = np.arange(
-                #         len(obj_list), dtype=np.int32)
-                #     annotations['class'] = np.array([
-                #         self.cat2label[obj.classname] for obj in obj_list
-                #         if obj.classname in self.cat2label.keys()
-                #     ])
-                #     annotations['gt_boxes_upright_depth'] = np.stack(
-                #         [
-                #             obj.box3d for obj in obj_list
-                #             if obj.classname in self.cat2label.keys()
-                #         ],
-                #         axis=0)  # (K,8)
-                annotations = {}
-                annotations['name'] = np.array([
-                             obj.classname for obj in obj_list
-                             if obj.classname in self.cat2label.keys()
-                         ])
-                annotations['label_cor']=np.array([obj_list])
-                info['annos'] = annotations
+            points = np.load(pts_filename).astype(np.float32)
+            pts_instance_mask = np.load(pts_instance_mask_path).astype(
+                np.int64)
+            pts_semantic_mask = np.load(pts_semantic_mask_path).astype(
+                np.int64)
+
+            mmengine.mkdir_or_exist(osp.join(self.root_dir, 'points'))
+            mmengine.mkdir_or_exist(osp.join(self.root_dir, 'instance_mask'))
+            mmengine.mkdir_or_exist(osp.join(self.root_dir, 'semantic_mask'))
+
+            points.tofile(
+                osp.join(self.root_dir, 'points',
+                         f'{self.split}_{sample_idx}.bin'))
+            pts_instance_mask.tofile(
+                osp.join(self.root_dir, 'instance_mask',
+                         f'{self.split}_{sample_idx}.bin'))
+            pts_semantic_mask.tofile(
+                osp.join(self.root_dir, 'semantic_mask',
+                         f'{self.split}_{sample_idx}.bin'))
+
+            info['pts_path'] = osp.join('points',
+                                        f'{self.split}_{sample_idx}.bin')
+            info['pts_instance_mask_path'] = osp.join(
+                'instance_mask', f'{self.split}_{sample_idx}.bin')
+            info['pts_semantic_mask_path'] = osp.join(
+                'semantic_mask', f'{self.split}_{sample_idx}.bin')
+            #process single scene rgb-image and depth-image
+            img_path = osp.join(self.split,sample_idx, f'{sample_idx}_rgb.png')#need
+            image_info = {
+                # 'image_idx': sample_idx,
+                # 'image_shape': self.get_image_shape(sample_idx),
+                'img_path': img_path
+            }
+            info['images'] = image_info
+            depth_path=osp.join(self.split,sample_idx,f"{sample_idx}_depth.png")
+            depth_info={
+                "depth_image_path":depth_path
+            }
+            info["depth"]=depth_info
+            info['annos'] = self.get_bboxes(points, pts_instance_mask,
+                                            pts_semantic_mask)
+
             return info
-
-        sample_id_list = sample_id_list if \
-            sample_id_list is not None else self.sample_id_list
+        sample_id_list = sample_id_list if sample_id_list is not None \
+            else self.sample_id_list
         with futures.ThreadPoolExecutor(num_workers) as executor:
             infos = executor.map(process_single_scene, sample_id_list)
         return list(infos)
+
+    def get_bboxes(self, points, pts_instance_mask, pts_semantic_mask):
+        """Convert instance masks to axis-aligned bounding boxes.
+
+        Args:
+            points (np.array): Scene points of shape (n, 6).
+            pts_instance_mask (np.ndarray): Instance labels of shape (n,).
+            pts_semantic_mask (np.ndarray): Semantic labels of shape (n,).
+
+        Returns:
+            dict: A dict containing detection infos with following keys:
+
+                - gt_boxes_upright_depth (np.ndarray): Bounding boxes
+                    of shape (n, 6)
+                - class (np.ndarray): Box labels of shape (n,)
+                - gt_num (int): Number of boxes.
+        """
+        bboxes, labels = [], []
+        for i in range(1, pts_instance_mask.max() + 1):
+            ids = pts_instance_mask == i
+            # check if all instance points have same semantic label
+            assert pts_semantic_mask[ids].min() == pts_semantic_mask[ids].max()
+            label = pts_semantic_mask[ids][0]
+            # keep only furniture objects
+            if label in self.cat_ids2class:
+                labels.append(self.cat_ids2class[pts_semantic_mask[ids][0]])
+                pts = points[:, :3][ids]
+                min_pts = pts.min(axis=0)
+                max_pts = pts.max(axis=0)
+                locations = (min_pts + max_pts) / 2
+                dimensions = max_pts - min_pts
+                bboxes.append(np.concatenate((locations, dimensions)))
+        annotation = dict()
+        # follow ScanNet and SUN RGB-D keys
+        annotation['gt_boxes_upright_depth'] = np.array(bboxes)
+        annotation['class'] = np.array(labels)
+        annotation['gt_num'] = len(labels)
+        return annotation
+
+
+    # def get_image(self, idx):
+    #     img_filename = osp.join(self.image_dir, f'{idx}.png')
+    #     return mmcv.imread(img_filename)
+
+    # def get_image_shape(self, idx):
+    #     image = self.get_image(idx)
+    #     return np.array(image.shape[:2], dtype=np.int32)
+
+    # def get_depth(self, idx):
+    #     depth_filename = osp.join(self.depth_dir, f'{idx:06d}.mat')
+    #     depth = sio.loadmat(depth_filename)['instance']
+    #     return depth
+
+    # def get_calibration(self, idx):
+    #     calib_filepath = osp.join(self.calib_dir, f'{idx:06d}.txt')
+    #     lines = [line.rstrip() for line in open(calib_filepath)]
+    #     Rt = np.array([float(x) for x in lines[0].split(' ')])
+    #     Rt = np.reshape(Rt, (3, 3), order='F').astype(np.float32)
+    #     K = np.array([float(x) for x in lines[1].split(' ')])
+    #     K = np.reshape(K, (3, 3), order='F').astype(np.float32)
+    #     return K, Rt
+
+    # def get_label_objects(self, idx):
+    #     label_filename = osp.join(self.label_dir, f'{idx}.txt')
+    #     lines = [line.rstrip() for line in open(label_filename)]
+    #     # objects = [PanoramaInstance(line) for line in lines]
+    #     return lines
+
+class PanoramaSegData(object):
+    """S3DIS dataset used to generate infos for semantic segmentation task.
+
+    Args:
+        data_root (str): Root path of the raw data.
+        ann_file (str): The generated scannet infos.
+        split (str, optional): Set split type of the data. Default: 'train'.
+        num_points (int, optional): Number of points in each data input.
+            Default: 8192.
+        label_weight_func (function, optional): Function to compute the
+            label weight. Default: None.
+    """
+
+    def __init__(self,
+                 data_root,
+                 ann_file,
+                 split='Area_1',
+                 num_points=4096,
+                 label_weight_func=None):
+        self.data_root = data_root
+        self.data_infos = mmengine.load(ann_file)
+        self.split = split
+        self.num_points = num_points
+
+        self.all_ids = np.arange(13)  # all possible ids
+        self.cat_ids = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                 12])  # used for seg task
+        self.ignore_index = len(self.cat_ids)
+
+        self.cat_id2class = np.ones(
+            (self.all_ids.shape[0], ), dtype=np.int64) * self.ignore_index
+        for i, cat_id in enumerate(self.cat_ids):
+            self.cat_id2class[cat_id] = i
+
+        # label weighting function is taken from
+        # https://github.com/charlesq34/pointnet2/blob/master/scannet/scannet_dataset.py#L24
+        self.label_weight_func = (lambda x: 1.0 / np.log(1.2 + x)) if \
+            label_weight_func is None else label_weight_func
+
+    def get_seg_infos(self):
+        scene_idxs, label_weight = self.get_scene_idxs_and_label_weight()
+        save_folder = osp.join(self.data_root, 'seg_info')
+        mmengine.mkdir_or_exist(save_folder)
+        np.save(
+            osp.join(save_folder, f'{self.split}_resampled_scene_idxs.npy'),
+            scene_idxs)
+        np.save(
+            osp.join(save_folder, f'{self.split}_label_weight.npy'),
+            label_weight)
+        print(f'{self.split} resampled scene index and label weight saved')
+
+    def _convert_to_label(self, mask):
+        """Convert class_id in loaded segmentation mask to label."""
+        if isinstance(mask, str):
+            if mask.endswith('npy'):
+                mask = np.load(mask)
+            else:
+                mask = np.fromfile(mask, dtype=np.int64)
+        label = self.cat_id2class[mask]
+        return label
+
+    def get_scene_idxs_and_label_weight(self):
+        """Compute scene_idxs for data sampling and label weight for loss
+        calculation.
+
+        We sample more times for scenes with more points. Label_weight is
+        inversely proportional to number of class points.
+        """
+        num_classes = len(self.cat_ids)
+        num_point_all = []
+        label_weight = np.zeros((num_classes + 1, ))  # ignore_index
+        for data_info in self.data_infos:
+            label = self._convert_to_label(
+                osp.join(self.data_root, data_info['pts_semantic_mask_path']))
+            num_point_all.append(label.shape[0])
+            class_count, _ = np.histogram(label, range(num_classes + 2))
+            label_weight += class_count
+
+        # repeat scene_idx for num_scene_point // num_sample_point times
+        sample_prob = np.array(num_point_all) / float(np.sum(num_point_all))
+        num_iter = int(np.sum(num_point_all) / float(self.num_points))
+        scene_idxs = []
+        for idx in range(len(self.data_infos)):
+            scene_idxs.extend([idx] * int(round(sample_prob[idx] * num_iter)))
+        scene_idxs = np.array(scene_idxs).astype(np.int32)
+
+        # calculate label weight, adopted from PointNet++
+        label_weight = label_weight[:-1].astype(np.float32)
+        label_weight = label_weight / label_weight.sum()
+        label_weight = self.label_weight_func(label_weight).astype(np.float32)
+
+        return scene_idxs, label_weight
+
+
+
