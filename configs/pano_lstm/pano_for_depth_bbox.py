7a0e57d6376f7bbb456075cc7234fc2fd90bbcf4
crzcreate <2452809832@qq.com>
2025-03-23 20:57:20 +0800

    Add files via upload

---
+++ configs/pano_lstm/pano_for_depth_bbox.py
@@ -0,0 +1,16 @@
+_base_ = [
+    '../_base_/datasets/panorama_3d.py', '../_base_/models/pano_depth_bbox.py',
+    '../_base_/schedules/seg-cosine-50e.py', '../_base_/default_runtime.py'
+]
+#这里进行修改过的
+# model settings
+
+
+# data settings
+train_dataloader = dict(batch_size=16)
+
+# runtime settings
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=2))
+
+# PointNet2-MSG needs longer training time than PointNet2-SSG
+train_cfg = dict(by_epoch=True, max_epochs=80, val_interval=2)
